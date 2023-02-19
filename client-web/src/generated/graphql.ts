cbcd7e035d0e3f25849b79ca0666a6b46fa9edd1
jason cheng <jasonvitagen@gmail.com>
2023-02-19 11:21:05 +0800

    feat: home page with graphql query

---
+++ client-web/src/generated/graphql.ts
@@ -0,0 +1,58 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Department = {
+  __typename?: 'Department';
+  dept_name: Scalars['String'];
+  id: Scalars['ID'];
+};
+
+export type Employee = {
+  __typename?: 'Employee';
+  birth_date: Scalars['String'];
+  department: Department;
+  first_name: Scalars['String'];
+  hire_date: Scalars['String'];
+  id: Scalars['ID'];
+  last_name: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allEmployees: Array<Employee>;
+  employeesByDepartment: Array<Employee>;
+};
+
+
+export type QueryAllEmployeesArgs = {
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type QueryEmployeesByDepartmentArgs = {
+  department: Scalars['String'];
+  limit?: InputMaybe<Scalars['Int']>;
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+export type GetEmployeesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetEmployeesQuery = { __typename?: 'Query', allEmployees: Array<{ __typename?: 'Employee', id: string, first_name: string, last_name: string, birth_date: string, department: { __typename?: 'Department', id: string, dept_name: string } }> };
+
+
+export const GetEmployeesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetEmployees"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"allEmployees"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"first_name"}},{"kind":"Field","name":{"kind":"Name","value":"last_name"}},{"kind":"Field","name":{"kind":"Name","value":"birth_date"}},{"kind":"Field","name":{"kind":"Name","value":"department"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"dept_name"}}]}}]}}]}}]} as unknown as DocumentNode<GetEmployeesQuery, GetEmployeesQueryVariables>;
--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,9 +1,20 @@
 import {createRoot} from "react-dom/client";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import App from "./App";
 
 const appElement = document.getElementById("app");
 
 if (appElement) {
+    const client = new ApolloClient({
+        uri: "http://localhost:3000/graphql",
+        cache: new InMemoryCache(),
+    });
+
     const root = createRoot(appElement);
 
-    root.render(<h3>Hello</h3>);
+    root.render(
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    );
 }
